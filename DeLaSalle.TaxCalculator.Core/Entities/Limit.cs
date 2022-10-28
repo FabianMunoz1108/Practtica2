20aff9b72836f2f3c26aaa341addd440e0380be5
Fabián Muñoz Tavares <fabian.munoz@congresogto.gob.mx>
2022-10-28 12:36:51 -0500

    Agregar archivos de proyecto.

---
+++ DeLaSalle.TaxCalculator.Core/Entities/Limit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeLaSalle.TaxCalculator.Core.Entities
+{
+    public class Limit
+    {
+        public double LowerLimit { get; set; }
+        public double UpperLimit { get; set; }
+        public double FixedFee { get; set; }
+        public double SurplusPercentage { get; set; }
+    }
+}
---
+++ DeLaSalle.TaxCalculator.Core/Entities/Salary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeLaSalle.TaxCalculator.Core.Entities
+{
+    public class Salary
+    {
+        public double Amount { get; set; }
+    }
+}
